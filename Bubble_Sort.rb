1306a8b2247c396e0e1d88d6ef513eb7aa2e66e8
rootDEV2990 <website.dev@icloud.com>
2020-05-11 15:17:33 -0500

    Autofix for rubocop and comment for test case

--- Bubble_Sort.rb
+++ Bubble_Sort.rb
@@ -1,17 +1,19 @@
 def bubble_sort(arr)
-    index = 0
-    while index < arr.length - 1 
-        (arr.length - 1).times do |i|
-            if arr[i] > arr[i + 1]
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-            else 
-                arr[i], arr[i + 1] = arr[i], arr[i + 1]
-            end
+  index = 0
+  while index < arr.length - 1
+    (arr.length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      else
+        arr[i] = arr[i]
+        arr[i + 1] = arr[i + 1]
         end
-        index += 1
     end
-    arr
+    index += 1
+  end
+  arr
 end
 
-arr = [5,4,6,8,4,2]
-bubble_sort(arr)
+#test case
+arr = [5, 4, 6, 8, 4, 2]
+p bubble_sort(arr)
